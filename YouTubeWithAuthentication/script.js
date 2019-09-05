bba645c13aa8ee678d9f1b136ecdcbf5d6504866
Alice Lee <alice880405@gmail.com>
2019-09-05 17:53:11 +0800

    chage the maxResults

--- YouTubeWithAuthentication/script.js
+++ YouTubeWithAuthentication/script.js
@@ -140,7 +140,7 @@ function requestVideoPlaylist(playlistId) {
     const requestOptions = {
       playlistId: playlistId,
       part: 'snippet',
-      maxResults: 10
+      maxResults: 12
     };
   
     const request = gapi.client.youtube.playlistItems.list(requestOptions);
